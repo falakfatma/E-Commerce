a6675148f903c2de05d367e1d455b2bdfa02cb18
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-10-21 09:06:42 +0000

    Navbar Style Is Added.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <ul className="navbar-menu">
-          <li><a href="#">Shop</a></li>
+          <li><a href="#" className="active">Shop</a></li>
           <li><a href="#">Men</a></li>
           <li><a href="#">Women</a></li>
           <li><a href="#">Kids</a></li>
