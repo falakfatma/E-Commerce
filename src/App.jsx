2d717b4b16551950eed5c27f32d1ec93433fa55f
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-10-21 08:56:42 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+export default function App() {
+  return (
+    <main>
+      {/* navbar
+      main = hero page
+      exclusive offer
+      new collection
+      featured products
+      footer */}
+      <Navbar />
+    </main>
+  )
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <ul className="navbar-menu">
+          <li><a href="#">Shop</a></li>
+          <li><a href="#">Men</a></li>
+          <li><a href="#">Women</a></li>
+          <li><a href="#">Kids</a></li>
+          <li><a href="#" className="LogIn">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
