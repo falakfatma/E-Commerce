f25fcad4f2ef8509dd6a2439ca36b52aadebda51
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-10-26 10:52:19 +0000

    Add Card

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,10 @@
+export default function Cards() {
+  return (
+    <div className="cards">
+      <div className="card">
+        <h3>Card 1</h3>
+        <p>Lorem ipsum dolor sit amet,   consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  )
+}
